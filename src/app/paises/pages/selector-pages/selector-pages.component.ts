cbb9f24061dbe58003c96f9ed709a937a7d39da6
XFabio22 <98523846+XFabio22@users.noreply.github.com>
2023-01-08 03:33:12 +0100

    selector pais bug fixed

--- src/app/paises/pages/selector-pages/selector-pages.component.ts
+++ src/app/paises/pages/selector-pages/selector-pages.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PaisesService } from '../../services/paises.service';
 import { PaisSmall } from '../../interfaces/paises.Interfaces';
+import { observable, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-pages',
@@ -25,16 +26,33 @@ export class SelectorPagesComponent  implements OnInit{
 
       //cuando cambie la region
 
-      this.miFormulario.get('region')?.valueChanges
-      .subscribe(region=>{
-        console.log(region);
-        this.paisesService.getPaisesPorRegion(region)
-        .subscribe(paises =>{
-            this.paises = paises
-            console.log(paises);
-            
-        })
+    
+      // this.miFormulario.get('region')?.valueChanges
+      // .subscribe(region=>{
+      //   console.log(region);
+      //   this.paisesService.getPaisesPorRegion(region)
+      //   .subscribe(paises =>{
+      //       this.paises = paises
+      //       console.log(paises);
+      //   })
+      // })
+
+      //el switchMap  toma el valor producto de un observable
+     // y a su vez lo muta y regresa el valor de otro observable
+
+      this.miFormulario.get('region')?.valueChanges //cada vez que region cambia 
+      .pipe(
+        tap( _ =>{ // lo detecto y hago un reset de el campo
+          this.miFormulario.get('pais')?.reset('')
+        }),
+        switchMap(region =>this.paisesService.getPaisesPorRegion(region))// vuelvo a busar la region
+      )
+      .subscribe(paises => { //lo meto al array
+        this.paises = paises
+        console.log(paises);
+        
       })
+
     }
     guardar(){
 
