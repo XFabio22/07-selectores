7ca1a4cfb3f89ea2907208d1782bd5a8837c5391
XFabio22 <98523846+XFabio22@users.noreply.github.com>
2023-01-08 01:26:29 +0100

    Selector

---
+++ src/app/paises/pages/selector-pages/selector-pages.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selector-pages',
+  templateUrl: './selector-pages.component.html',
+  styleUrls: ['./selector-pages.component.css']
+})
+export class SelectorPagesComponent {
+
+}
